94dd837f529fa5a32d5b3dbf1db1f9080974a645
mokegg <mekonnen.gebrehiwot1@gmail.com>
2021-12-17 12:59:35 +0100

    steam.py updated

--- Flask/steam.py
+++ Flask/steam.py
@@ -3,6 +3,6 @@ from app import app
 import os
 
 if __name__ == '__main__':
-    port = of.environ.get('PORT', 5000) #Heroku will set env variable to web traffic
+    port = os.environ.get('PORT', 5000) #Heroku will set env variable to web traffic
     app.run(host="0.0.0.0", debug=False)
 
